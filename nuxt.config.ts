a3df54f384b3fbeb0df3449b3cdd8f6e04c0db9a
Wren Etheredge <me@wetheredge.com>
2025-02-22 15:52:32 -0500

    Add prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  typescript: { typeCheck: true },
-  modules: ['@nuxt/eslint'],
-})
+	compatibilityDate: "2024-11-01",
+	devtools: { enabled: true },
+	typescript: { typeCheck: true },
+	modules: ["@nuxt/eslint"],
+});
