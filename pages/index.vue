12400f8fdc12360ff2a0f65fab67c35ea97a3847
Wren Etheredge <me@wetheredge.com>
2025-02-23 16:08:15 -0500

    Add geolocate & navigation controls

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,15 @@
 <template>
 	<!-- FIXME: default styles contain both `position: relative` & `absolute` at equal specificity with unpredicatable order -->
 	<MapboxMap
+		mapId="map"
 		style="position: absolute"
 		:options="{
 			style: 'mapbox://styles/mapbox/outdoors-v12',
 			center: [-68.137343, 45.137451],
 			zoom: 5,
 		}"
-	/>
+	>
+		<MapboxGeolocateControl position="bottom-left" />
+		<MapboxNavigationControl position="bottom-left" />
+	</MapboxMap>
 </template>
