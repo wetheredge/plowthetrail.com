fb6f2be073bdfbe92f1c0e2ebf2863be7678f9ad
Wren Etheredge <me@wetheredge.com>
2025-02-23 19:44:20 -0500

    Add togglable filters panel

---
+++ components/FilterPanel.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const emit = defineEmits<{
+	close: void;
+}>();
+
+const timePeriods = ["24h", "48h", "7d", "All"];
+const timePeriod = ref(timePeriods[0]);
+</script>
+
+<template>
+	<div class="relative z-10 w-fit m-4 ml-auto p-4 glass border rounded-md">
+		<div class="flex justify-between gap-2 mb-2">
+			<span class="font-semibold">Filters</span>
+			<UButton
+				color="gray"
+				variant="ghost"
+				icon="i-heroicons-x-mark"
+				@click="$emit('close')"
+			/>
+		</div>
+
+		<UFormGroup label="Time period:">
+			<UButtonGroup>
+				<UButton
+					v-for="period in timePeriods"
+					:label="period"
+					:color="period === timePeriod ? 'primary' : 'gray'"
+					@click="timePeriod = period"
+				/>
+			</UButtonGroup>
+		</UFormGroup>
+	</div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,21 @@
+<script setup lang="ts">
+const showFilters = ref(false);
+</script>
+
 <template>
 	<TopBar>
 		<UTooltip text="New report">
 			<UButton color="primary" variant="ghost" icon="i-heroicons-plus" />
 		</UTooltip>
+		<UTooltip text="Toggle filters panel">
+			<UButton
+				color="gray"
+				variant="ghost"
+				icon="i-heroicons-funnel"
+				@click="showFilters = !showFilters"
+			/>
+		</UTooltip>
 	</TopBar>
+	<FilterPanel v-if="showFilters" @close="showFilters = false" />
 	<TrailMap />
 </template>
